f009f75cdb4436fe42524e90accbd71ba6082c7b
pkondrashkov <pkondrashkov@gmail.com>
2021-10-11 12:50:27 +0300

    Release 5.8.59

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let sdkVersion = "0.1.16"
-let sdkChecksum = "45cf6898b4ebd461cb817a1f834f7a8d324d4e3b9071eaf29b91b9e9764e8b94"
+let sdkVersion = "5.8.59"
+let sdkChecksum = "568eb8848b7e487792cf29f0f926ba7756159bbc547f8822cb1a0d69a54d1836"
 
 let package = Package(
     name: "RegulaCommon",
