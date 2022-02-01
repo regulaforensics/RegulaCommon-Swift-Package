e5cfd39858c3f00c13cd4615bd6003ee33120bbf
pkondrashkov <pkondrashkov@Pavels-MacBook-Pro.local>
2022-02-01 14:52:58 +0300

    bumped iOS version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let sdkVersion = "5.8.59"
-let sdkChecksum = "568eb8848b7e487792cf29f0f926ba7756159bbc547f8822cb1a0d69a54d1836"
+let sdkVersion = "template"
+let sdkChecksum = "template"
 
 let package = Package(
     name: "RegulaCommon",
+    platforms: [.iOS(.v11)],
     products: [
         .library(name: "RegulaCommon", targets: ["RegulaCommon"])
     ],
