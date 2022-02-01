4a707d122f92dd255d26b789d827c3589317e646
pkondrashkov <pkondrashkov@Pavels-MacBook-Pro.local>
2022-02-01 15:32:25 +0300

    Release 6.1.102

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let sdkVersion = "5.8.59"
-let sdkChecksum = "568eb8848b7e487792cf29f0f926ba7756159bbc547f8822cb1a0d69a54d1836"
+let sdkVersion = "6.1.102"
+let sdkChecksum = "3d84f7a68029d4592cb1f707f7332a7cc62193b3dd8f81f897a5357c3a15aabf"
 
 let package = Package(
     name: "RegulaCommon",
+    platforms: [.iOS(.v11)],
     products: [
         .library(name: "RegulaCommon", targets: ["RegulaCommon"])
     ],
