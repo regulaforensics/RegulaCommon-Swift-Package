969455b01866040124bbe7461bdeb49808c298e7
jenkins <jenkins@regula.by>
2024-08-01 21:10:09 +0300

    Release 7.2.487

--- Package.swift
+++ Package.swift
@@ -1,20 +1,15 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let sdkVersion = "6.1.102"
-let sdkChecksum = "3d84f7a68029d4592cb1f707f7332a7cc62193b3dd8f81f897a5357c3a15aabf"
-
 let package = Package(
     name: "RegulaCommon",
     platforms: [.iOS(.v11)],
     products: [
-        .library(name: "RegulaCommon", targets: ["RegulaCommon"])
+        .library(
+            name: "RegulaCommon",
+            targets: ["RegulaCommon"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "RegulaCommon",
-            url: "https://pods.regulaforensics.com/RegulaCommon/\(sdkVersion)/RegulaCommon-\(sdkVersion).zip",
-            checksum: sdkChecksum
-        )
+        .binaryTarget(name: "RegulaCommon", url: "https://pods.regulaforensics.com/RegulaCommon/7.2.487/RegulaCommon-7.2.487.zip", checksum: "79e401a8cfc0a16751479bccfca645709037e0afd6c78f653b765fc846ad0220"),
     ]
 )
