6ebe790dce5dfa74f1b9d52648d12cfaf950c459
jenkins <jenkins@regula.by>
2025-03-29 00:14:19 +0300

    Release 7.6.1074

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["RegulaCommon"]),
     ],
     targets: [
-        .binaryTarget(name: "RegulaCommon", url: "https://pods.regulaforensics.com/RegulaCommon/7.5.935/RegulaCommon-7.5.935.zip", checksum: "44e75f3b3429f6f27e71a6120ac40d8bd05e668126dbd4ab692dbf7ce3570e35"),
+        .binaryTarget(name: "RegulaCommon", url: "https://pods.regulaforensics.com/RegulaCommon/7.6.1074/RegulaCommon-7.6.1074.zip", checksum: "23a5beaa598fe5d6efd28e8b1e8054e40505f5fe961d6395bea8c2690aaa8b14"),
     ]
 )
