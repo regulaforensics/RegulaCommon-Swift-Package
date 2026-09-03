8d5379b756522ae795b855f631078dc200915088
Jenkins <jenkins@regula.by>
2026-09-03 15:27:58 +0300

    RC 9.8.2758

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["RegulaCommonStage"]),
     ],
     targets: [
-        .binaryTarget(name: "RegulaCommonStage", url: "https://pods.regulaforensics.com/Stage/RegulaCommonStage/9.8.2757/RegulaCommonStage-9.8.2757.zip", checksum: "f92d5fd50e7e835d1c8161714ab7ab81db2d5245b2bceb32c67ea8e8d09f2bb3"),
+        .binaryTarget(name: "RegulaCommonStage", url: "https://pods.regulaforensics.com/Stage/RegulaCommonStage/9.8.2758/RegulaCommonStage-9.8.2758.zip", checksum: "a2f5abadacd37b357ce03593cda5a2582c5849e8849fa5a69030b9cb3adae584"),
     ]
 )
