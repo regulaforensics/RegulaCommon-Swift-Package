f2ca91a04c81d72286cb8ffa730b7f1a0da20908
Jenkins <jenkins@regula.by>
2026-09-15 01:09:59 +0300

    Nightly 9.9.2809

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     products: [
         .library(
             name: "RegulaCommon",
-            targets: ["RegulaCommonBeta"]),
+            targets: ["RegulaCommonNightly"]),
     ],
     targets: [
-        .binaryTarget(name: "RegulaCommonBeta", url: "https://pods.regulaforensics.com/RegulaCommonBeta/9.9.2808/RegulaCommonBeta-9.9.2808.zip", checksum: "4b8e87e096ded6bbacb3b0bd5faaf7ad0cbdfac95db74cbd426a8a03371c67e6"),
+        .binaryTarget(name: "RegulaCommonNightly", url: "https://pods.regulaforensics.com/Nightly/RegulaCommonNightly/9.9.2809/RegulaCommonNightly-9.9.2809.zip", checksum: "1f4f461c365543c233ca0008da9479c6d31ed2f5bda9552e4d834e4671518bf6"),
     ]
 )
