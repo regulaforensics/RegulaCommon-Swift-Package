3f01d686c2f9052313ff08f18d5b5a0747393f94
Jenkins <jenkins@regula.by>
2026-09-25 14:14:32 +0300

    Nightly 9.9.2873

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "RegulaCommon",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "RegulaCommon",
-            targets: ["RegulaCommonStage"]),
+            targets: ["RegulaCommonNightly"]),
     ],
     targets: [
-        .binaryTarget(name: "RegulaCommonStage", url: "https://pods.regulaforensics.com/Stage/RegulaCommonStage/9.8.2870/RegulaCommonStage-9.8.2870.zip", checksum: "32e6e439f38581a9d253cef92b45eade14a446246c3096e4fdc557005e41d2ce"),
+        .binaryTarget(name: "RegulaCommonNightly", url: "https://pods.regulaforensics.com/Nightly/RegulaCommonNightly/9.9.2873/RegulaCommonNightly-9.9.2873.zip", checksum: "a1aae3a62a763af03a820da5de8ba79f80ad3c3995c0dfbf150512106b1b85bc"),
     ]
 )
